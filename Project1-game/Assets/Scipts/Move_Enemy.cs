d9a32e0a52c1fc6f5fc2e5bf9fab250edf118aaf
Hannes Pongratz <pongratz.hannes@gmail.com>
2018-11-17 16:57:41 +0100

    Move für Enemy 1%

---
+++ Project1-game/Assets/Scipts/Move_Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Enemy : MonoBehaviour {
+    public float Move_Speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
