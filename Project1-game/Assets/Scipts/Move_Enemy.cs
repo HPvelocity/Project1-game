4f19910b00c8a8297034d1cbbea563bca8f64fd1
Hannes Pongratz <pongratz.hannes@gmail.com>
2018-11-18 17:09:17 +0100

    Move Enemy/Shoot 70%

--- Project1-game/Assets/Scipts/Move_Enemy.cs
+++ Project1-game/Assets/Scipts/Move_Enemy.cs
@@ -3,16 +3,124 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move_Enemy : MonoBehaviour {
+    private GameObject Main_Player;
     public float Move_Speed;
+    public int Distance_To_Player_DownUp;
+    public float Distance_To_Player_LeftRight;
+    public float Move_Delay = 1;
 
+    struct Where_Is_Player
+    {
+        public bool Player_Down, Player_Up, Player_Right, Player_Left;
+    }
+    Where_Is_Player Player_Is_There = new Where_Is_Player();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Rigidbody2D rig_From_Enemy;
+
+    // Use this for initialization
+    void Start () {
+        rig_From_Enemy = this.GetComponent<Rigidbody2D>();
+        Main_Player = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (MainPlayer_Far())
+        {
+            if (Player_Is_There.Player_Down)
+            {
+                Move_Down();
+            }else if (Player_Is_There.Player_Up)
+            {
+                Move_Up();
+            }
+
+            if (Player_Is_There.Player_Right)
+            {
+                Move_Right();
+            }else if (Player_Is_There.Player_Left)
+            {
+                Move_Left();
+            }
+        }
 	}
+
+    private void Move_Right()
+    {
+        Vector3 vec3 = rig_From_Enemy.velocity;
+        vec3.x = Move_Speed * Time.deltaTime;
+        Transform transform = this.transform;
+        transform.Translate(vec3);
+    }
+
+    private void Move_Left()
+    {
+        Vector3 vec3 = rig_From_Enemy.velocity;
+        vec3.x -= Move_Speed * Time.deltaTime;
+        Transform transform = this.transform;
+        transform.Translate(vec3);
+    }
+
+    private void Move_Down()
+    {
+        Vector3 vec3 = rig_From_Enemy.velocity;
+        vec3.y -= Move_Speed * Time.deltaTime;
+        Transform transform = this.transform;
+        transform.Translate(vec3);
+    }
+
+    private void Move_Up()
+    {
+        Vector3 vec3 = rig_From_Enemy.velocity;
+        vec3.y = Move_Speed * Time.deltaTime;
+        Transform transform = this.transform;
+        transform.Translate(vec3);
+    }
+
+    private bool MainPlayer_Far()
+    {
+        bool Player_Far = false;
+
+        if (this.transform.position.y - Distance_To_Player_DownUp >  Main_Player.transform.position.y)
+        {
+            Player_Is_There.Player_Down = true;
+            Player_Far = true;
+        }
+        else
+        {
+            Player_Is_There.Player_Down = false;
+        }
+        if (this.transform.position.y < Main_Player.transform.position.y)
+        {
+            Player_Is_There.Player_Up = true;
+            Player_Far = true;
+        }
+        else
+        {
+            Player_Is_There.Player_Up = false;
+        }
+        if (this.transform.position.x + Distance_To_Player_LeftRight < Main_Player.transform.position.x)
+        {
+            Player_Is_There.Player_Right = true;
+            Player_Far = true;
+        }
+        else
+        {
+            Player_Is_There.Player_Right = false;
+        }
+        if (this.transform.position.x - Distance_To_Player_LeftRight > Main_Player.transform.position.x)
+        {
+            Player_Is_There.Player_Left = true;
+            Player_Far = true;
+        }
+        else
+        {
+            Player_Is_There.Player_Left = false;
+        }
+
+
+        return Player_Far;
+    }
+
 }
---
+++ Project1-game/Assets/Scipts/Shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public GameObject Shoot_Object;
+    public float Shoot_Delay;
+    public GameObject Shoot_Point;
+
+    bool Shoot_Break = false;
+    bool Shoot_Delay_Finished = false;
+    bool Is_Corouting_Ready = true;
+
+    // Update is called once per frame
+    void Update() {
+        if (!Shoot_Delay_Finished && Is_Corouting_Ready)
+        {
+            Is_Corouting_Ready = false;
+            StartCoroutine(Start_Delay());
+        }
+        if (!Shoot_Break && Shoot_Delay_Finished)
+        {
+            Shoot_Delay_Finished = false;
+            Erzeuge_Shot();
+        }
+
+    }
+
+    private void Erzeuge_Shot()
+    {
+        Vector3 vec3 = Shoot_Point.transform.position;
+
+        //Vector3 End_Vec3 = Camera.main.ScreenToViewportPoint(vec3);
+        Instantiate(Shoot_Object, vec3, Quaternion.identity);
+    }
+
+   IEnumerator Start_Delay()
+    {
+        yield return new WaitForSeconds(Shoot_Delay);
+        Shoot_Delay_Finished = true;
+        Is_Corouting_Ready = true;
+    }
+}
