53a30aa53526049cd3075a34fc9a5eb5edf8f020
BonecoJan <w.jan77@web.de>
2018-11-28 18:10:42 +0100

    ShootController 1%

---
+++ Project1-game/Assets/Scipts/ShootController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+
+    public GameObject shoot;
+    public Transform[] ShootPoints;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (Transform current in ShootPoints)
+            {
+                Quaternion qu = current.rotation;
+
+                qu.z = 180;
+
+                Instantiate(shoot, current.position, qu);
+            }
+        }
+
+	}
+}
